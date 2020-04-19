1c099a473cd67f39dca284e28dd230bbe52cfdb3
irinabey88 <irinabey88@gmail.com>
2020-04-19 22:25:46 +0300

    Added task for module#4 debugging.

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using CrackMe;
+using KeyGenerator;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeysGenerator keyGenerator = new KeysGenerator();
+
+            Console.WriteLine(keyGenerator.GenerateKey());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Debugging/KeyGenerator/KeysGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace KeyGenerator
+{
+    public class KeysGenerator
+    {
+        public string GenerateKey()
+        {
+            byte[] address = NetworkInterface.GetAllNetworkInterfaces()
+                                             .FirstOrDefault()
+                                             .GetPhysicalAddress()
+                                             .GetAddressBytes();
+            byte[] date = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            IEnumerable<int> result = address.Select((item, i) => item ^ date[i])
+                                             .Select(item => item * 10);
+
+            return string.Join("-", result);
+        }
+    }
+}
